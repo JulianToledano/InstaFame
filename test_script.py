a1f91890d4812676a65378b48d18b6bc84a1f030
SergioRosello <sergio-rosello@hotmail.com>
2020-04-09 22:04:02 +0200

    First POC commit

---
+++ test_script.py
@@ -0,0 +1,34 @@
+""" Quickstart script for InstaPy usage """
+# imports
+from instapy import InstaPy
+from instapy import smart_run
+from dotenv import load_dotenv
+import os
+
+# Made to load env variables in .env
+load_dotenv()
+
+# login credentials
+insta_username = os.getenv('USERNAME')
+insta_password = os.getenv('PASSWORD')
+
+# get an InstaPy session!
+# set headless_browser=True to run InstaPy in the background
+session = InstaPy(username=insta_username,
+                  password=insta_password,
+                  headless_browser=False)
+
+with smart_run(session):
+    """ Activity flow """
+    # general settings
+    session.set_relationship_bounds(enabled=True,
+                                    delimit_by_numbers=True,
+                                    max_followers=4590,
+                                    min_followers=45,
+                                    min_following=77)
+
+    session.set_dont_include(["friend1", "friend2", "friend3"])
+    session.set_dont_like(["pizza", "#store"])
+
+    # activity
+    session.like_by_tags(["natgeo"], amount=10)
